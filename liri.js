4e1ad3cb35dbc827f9da85f5620796bab3d48f55
Kristofernash <39805142+Kristofernash@users.noreply.github.com>
2018-07-12 23:38:28 -0700

    homework

---
+++ liri.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+//Add the code required to import the `keys.js` file and store it in a variable.
+var keys = require("./keys");
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+
+
+
+
